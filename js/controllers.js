d2487105f62963f3fc782d942f21d921bc0df708
Gavin Leake <gleake@gmail.com>
2013-07-16 11:54:17 +0100

    two random awards and the actual, shuffled array

--- js/controllers.js
+++ js/controllers.js
@@ -2,6 +2,22 @@ function AwardListCtrl($scope, $http) {
   $http.get('data/chs2013csawards.json').success(function(data) {
     leng = data.length;
     rand = Math.floor((Math.random()*leng)+1);
-    $scope.awards = data.splice(rand, 1);
+    awards = data.splice(rand, 1);
+    values = [
+        awards[0]['award_made'],
+        data.splice(Math.floor((Math.random()*leng)+1), 1)[0]['award_made'],
+        data.splice(Math.floor((Math.random()*leng)+1), 1)[0]['award_made']
+    ];
+    $scope.awards = awards;
+    $scope.values = shuffle(values);
+    /*console.log(awards[0]['award_made']);
+    console.log(values);*/
   });
 }
+
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/array/shuffle [v1.0]
+function shuffle(o){ //v1.0
+    for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
