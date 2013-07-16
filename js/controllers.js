822cea3c1fe2eb4ae263a36ff153007190355fb7
Gavin Leake <gleake@gmail.com>
2013-07-16 11:29:43 +0100

    random award, list town and council

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,7 @@
 function AwardListCtrl($scope, $http) {
   $http.get('data/chs2013csawards.json').success(function(data) {
-    $scope.awards = data.splice(0, 5);
+    leng = data.length;
+    rand = Math.floor((Math.random()*leng)+1);
+    $scope.awards = data.splice(rand, 1);
   });
 }
